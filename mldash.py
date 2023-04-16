72073d4b9953ab3ff286a820f82a12ca9fb693a9
helenaaa1986 <h.bashardoost@gmail.com>
2023-04-16 11:39:57 -0400

    last version

--- mldash.py
+++ mldash.py
@@ -1,14 +1,24 @@
 from flask import Flask,render_template,request
 import pickle
+import numpy as np
 
 app = Flask(__name__)
-
+model = pickle.load(open('model.pkl','rb'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
-
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    temperature = float(request.form.get('temperature'))
+    prediction = model.predict(np.array([[temperature]]))
+    output = round(prediction[0], 2)
+    prediction_text = f'Total revenue generated is Rs.{output}/-'
+    return render_template('index1.html', prediction_text=prediction_text)
 
 if __name__ =='__main__':
     app.run(debug=True)
+
+
+
